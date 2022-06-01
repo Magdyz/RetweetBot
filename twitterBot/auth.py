a0182bfaa59d95ed9a37cceaf1cc7c1f63e8ac5c
Magz <79382097+Magdyz@users.noreply.github.com>
2022-06-01 13:47:22 +0100

    added notes

--- twitterBot/auth.py
+++ twitterBot/auth.py
@@ -1,3 +1,6 @@
+# All these keys could be grabbed from Twitter Api deveoper account. google how to get them
+# You can't get to google API with Authentication. 
+# Don't change the name of the file because it's used in the main code
 
 consumer_key = "#########################"
 consumer_secret = "##################################################"
